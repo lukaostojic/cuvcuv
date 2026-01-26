dc3964fbc344c08121f3e8f957bbe06e01ea7e21
Luka Ostojic <ostojaq842@gmail.com>
2026-01-26 20:47:46 +0100

    Mobile :: basic header setup

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,17 +1,56 @@
 <template>
-  <nav class="navigation">
-		<ul>
-			<li><RouterLink to="/">Home</RouterLink></li>
-			<li><RouterLink to="/about">About</RouterLink></li>
-			<li><RouterLink to="/upcoming-events">Upcoming Events</RouterLink></li>
-			<li><RouterLink to="/articles">Articles</RouterLink></li>
-			<li><RouterLink to="/releases">Releases</RouterLink></li>
-			<li><RouterLink to="/contact">Contacts</RouterLink></li>
+  <nav class="navigation p-relative full-width">
+		<div
+			@click="toggleNavigation()" 
+			:class="{ 'active': isNavigationVisible }"
+			class="navigation__hamburger d-flex-column justify-center"
+		>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+		<ul
+			class="navigation__list p-relative full-width full-vh px-lg py-lg"
+			:class="{ 'visible': isNavigationVisible }"
+		>
+			<li class="mb-md"><RouterLink to="/">Home</RouterLink></li>
+			<li class="mb-md"><RouterLink to="/about">About</RouterLink></li>
+			<li class="mb-md"><RouterLink to="/upcoming-events">Upcoming Events</RouterLink></li>
+			<li class="mb-md"><RouterLink to="/articles">Articles</RouterLink></li>
+			<li class="mb-md"><RouterLink to="/releases">Releases</RouterLink></li>
+			<li class="mb-md"><RouterLink to="/contact">Contacts</RouterLink></li>
 		</ul>
 	</nav>
+	<header class="header p-relative"></header>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default defineComponent({
+	name: 'Header',
+	setup() {
+		const isNavigationVisible = ref(false)
+		const route = useRoute()
+
+		const toggleNavigation = () => {
+			isNavigationVisible.value = !isNavigationVisible.value
+		}
+
+		watch(
+			() => route.fullPath,
+			() => {
+				isNavigationVisible.value = false
+			}
+		)
+
+		return {
+			isNavigationVisible,
+			toggleNavigation
+		}
+	}
+})
 
 </script>
 
